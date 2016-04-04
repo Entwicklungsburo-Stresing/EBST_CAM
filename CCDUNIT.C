126dcebbb1d6ae55474efeb5b6795dc570ed5a0c
Bastie <bastian.brabec@posteo.de>
2016-04-04 17:16:46 +0200

    tempbuf to display

--- CCDUNIT.C
+++ CCDUNIT.C
@@ -163,7 +163,7 @@ CopytoDispbuf()
 {	//display buffer is long
 	//data array is word
 	int i;
-
+	PUSHORT tempBuf = &DMAUserBuf[0][0];
 	//while (!GetNextScan){ Sleep(5); }
 	testcnt++;
 	if (testcnt == 100){
@@ -172,7 +172,7 @@ CopytoDispbuf()
 	}
 	for (i = 0; i < _PIXEL; i++){
 		//pDMABuf++;
-		DisplData[0][i] = *(pDMAUserBuf  + i*100);//DIODENRingBuf[i + 0*FirstPageOffset + 0 * RAMPAGESIZE];//20: its a random number of the Ringbuffer (max 99)
+		DisplData[0][i] = *(tempBuf  + i*100);//DIODENRingBuf[i + 0*FirstPageOffset + 0 * RAMPAGESIZE];//20: its a random number of the Ringbuffer (max 99)
 	}
 	//pDMABuf -= _PIXEL;
 	for (i = 0; i < _PIXEL; i++)
