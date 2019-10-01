ab44d4b9c049dda30300ca8d65d9efa64acc33c8
Florian <florian.hahn@posteo.de>
2019-10-01 14:16:05 +0200

    add direct2dViewer window to WinMain

---
+++ Direct2dViewer_c.cpp
@@ -0,0 +1,17 @@
+#include "Direct2dViewer_c.h"
+#include "Direct2dViewer.h"
+
+void* Direct2dViewer_new() {
+	return new Direct2dViewer();
+}
+
+void Direct2dViewer_delete(void *D2dV) {
+	Direct2dViewer *D2d = (Direct2dViewer *)D2dV;
+	delete D2d;
+}
+
+void Direct2dViewer_Initialize(void* D2dV) {
+	Direct2dViewer *D2d = (Direct2dViewer *)D2dV;
+	D2d->Initialize();
+}
+
--- Direct2dViewer_c.h
+++ Direct2dViewer_c.h
@@ -1,14 +1,16 @@
 #pragma once
 
-//cpp_connector.h is an example for a wrapper of cpp to provide c api
+//cpp_connector.h is a wrapper of Direct2dViewer.cpp to provide c api
 //inspired by this tutorial to call cpp methods from c: https://bytes.com/topic/c/insights/921728-calling-c-class-methods-c
+
 
 #ifdef __cplusplus
 extern "C"
 {
 #endif
-	void* testcpp_new();
-	void createNewWindow_c(void* testcpp);
+	void* Direct2dViewer_new();
+	void Direct2dViewer_Initialize(void* D2dV);
+	void Direct2dViewer_delete(void *D2dV);
 
 #ifdef __cplusplus    
 }
