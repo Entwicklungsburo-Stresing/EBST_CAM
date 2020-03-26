91450ece7591dab348115668e23dacd55695ebdd
Florian <florian.hahn@posteo.de>
2020-03-26 14:46:50 +0100

    cosmetics

--- ESLSCDLL/ESLSCDLL.h
+++ ESLSCDLL/ESLSCDLL.h
@@ -49,13 +49,13 @@ void	*dummy;
 
 volatile struct ffloopparams params, params2;
 
+BOOL WINAPI DLLMain( HINSTANCE hInstDLL, DWORD dwNotification, LPVOID lpReserved );
 DllAccess int DLLGetProcessCount();
 DllAccess int DLLGetThreadCount();
 DllAccess  void DLLErrMsgBoxOn( void );	//BOARD.C sends error messages on default
 DllAccess  void DLLErrMsgBoxOff( void );	//general deactivate of error message boxes
 DllAccess UINT8 nDLLCCDDrvInit( void );		// init the driver -> true if found
 DllAccess void DLLCCDDrvExit( UINT32 drv );		// closes the driver
-DllAccess void DLLReturnFrame( UINT32 drv, UINT32 curr_nos, UINT32 curr_nob, UINT16 curr_cam, UINT16 *pdioden, UINT32 length );
 DllAccess UINT8 n2DLLInitBoard( UINT32 drv, UINT32 camcnt, UINT32 pixel, UINT32 flag816, UINT32 pclk, UINT32 xckdelay );		// init the driver -> true if found
 DllAccess UINT8 DLLReadByteS0( UINT32 drv, UINT8 *data, UINT32 PortOff );// read byte from Port, PortOff = Regs of Board
 DllAccess UINT8 DLLWriteByteS0( UINT32 drv, UINT8 DataByte, UINT32 PortOff ); // writes DataByte to Port
@@ -75,25 +75,29 @@ DllAccess void DLLBothSlope( UINT32 drv );	//trigger on each slope
 DllAccess void DLLOutTrigHigh( UINT32 drv );		//set output Trigger signal high
 DllAccess void DLLOutTrigLow( UINT32 drv );		//set output Trigger signal low
 DllAccess void DLLOutTrigPulse( UINT32 drv, UINT32 PulseWidth );	// pulses high output Trigger signal
-DllAccess UINT8 DLLSetS0Bit( ULONG bitnumber, CHAR Address, UINT32 drvno );
-DllAccess UINT8 DLLResetS0Bit( ULONG bitnumber, CHAR Address, UINT32 drvno );
 DllAccess void DLLOpenShutter( UINT32 drv );	// set IFC=high
 DllAccess void DLLCloseShutter( UINT32 drv );	// set IFC=low
 //************ FIFO version functions
-DllAccess void DLLSetupVCLK( UINT32 drvno, UINT32 lines, UINT8 vfreq );//set the VCLK regs
 DllAccess void DLLSWTrig( UINT32 drvno );						//start a read to FIFO by software
 DllAccess UINT8 DLLFFValid( UINT32 drvno );						// TRUE if linecounter>0
 DllAccess void DLLSetExtTrig( UINT32 drvno );					// read to FIFO is triggered by external input I of PCI board
 DllAccess void DLLSetIntTrig( UINT32 drvno );					// read to FIFO is triggered by Timer
 DllAccess UINT8 DLLFFOvl( UINT32 drvno );					//TRUE if fifo overflow occured
+DllAccess void DLLSetupVCLK( UINT32 drvno, UINT32 lines, UINT8 vfreq );//set the VCLK regs
 DllAccess void DLLReadRingLine( pArrayT pdioden, UINT32 lno ); //read ring buffer line number lno 
 DllAccess UINT8 DLLBlockTrig( UINT32 drv, UCHAR btrig_ch ); //read trigger input ->ch=1:pci in, ch=2:opto1, ch=3:opto2
 //************ camera reads FIFO version
-DllAccess void nDLLReadFFLoop( UINT32 board_sel, UINT32 exptus, UINT8 exttrig, UINT8 blocktrigger, UINT8 btrig_ch );
-DllAccess void DLLStopFFLoop( void );
+DllAccess UINT8 DLLSetS0Bit( ULONG bitnumber, CHAR Address, UINT32 drvno );
+DllAccess UINT8 DLLResetS0Bit( ULONG bitnumber, CHAR Address, UINT32 drvno );
 //************ system timer
 DllAccess UINT64 DLLTicksTimestamp( void );
 DllAccess UINT32 DLLTickstous( UINT64 tks );
+DllAccess void DLLSetupDMA( UINT32 drv, void*  pdioden, UINT32 nos, UINT32 blocks );
+DllAccess void nDLLSetupDMA( UINT32 drv, UINT32 nos, UINT32 blocks );
+DllAccess void DLLReturnFrame( UINT32 drv, UINT32 curr_nos, UINT32 curr_nob, UINT16 curr_cam, UINT16 *pdioden, UINT32 length );
+DllAccess void nDLLReadFFLoop( UINT32 board_sel, UINT32 exptus, UINT8 exttrig, UINT8 blocktrigger, UINT8 btrig_ch );
+DllAccess void DLLStopFFLoop( void );
+DllAccess void DLLSetContFFLoop( UINT8 activate );
 //************  Cooling
 DllAccess void DLLSetTemp( UINT32 drvno, UINT8 level );
 DllAccess void DLLSetEC( UINT32 drvno, UINT64 ecin100ns );
@@ -101,14 +105,16 @@ DllAccess void DLLResetEC( UINT32 drvno );
 DllAccess void DLLSetTORReg( UINT32 drvno, UINT8 fkt );
 DllAccess void DLLSetupDELAY( UINT32 drvno, UINT32 delay );
 DllAccess void DLLSetISPDA( UINT32 drvno, UINT8 set );
-DllAccess void DLLSetISFFT( UINT32 drvno, UINT8 set );
 DllAccess void DLLSetPDAnotFFT( UINT32 drvno, UINT8 set );
+DllAccess void DLLSetISFFT( UINT32 drvno, UINT8 set );
 DllAccess void DLLRsTOREG( UINT32 drvno );
 DllAccess void DLLSetupHAModule( UINT8 irsingle, UINT32 fftlines );
 DllAccess void DLLSetupVPB(UINT32 drvno, UINT32 range, UINT32 lines, UINT8 keep);
 DllAccess void DLLSetupROI(UINT32 drvno, UINT16 number_of_regions, UINT32 lines, UINT8 keep_first, UINT8* region_size);
-DllAccess void DLLSetupDMA( UINT32 drv, void*  pdioden, UINT32 nos, UINT32 blocks );
-DllAccess void nDLLSetupDMA( UINT32 drv, UINT32 nos, UINT32 blocks );
+DllAccess void DLLAboutS0( UINT32 drv );
+DllAccess void DLLSendFLCAM( UINT32 drvno, UINT8 maddr, UINT8 adaddr, UINT16 data );
+DllAccess void DLLSendFLCAM_DAC( UINT32 drvno, UINT8 ctrl, UINT8 addr, UINT16 data, UINT8 feature );
+DllAccess void DLLFreeMemInfo( UINT64 memory_all, UINT64 memory_free );
 DllAccess void DLLErrorMsg( char ErrMsg[20] );
 DllAccess void DLLCalcTrms( UINT32 drvno, UINT32 nos, ULONG TRMSpix, UINT16 CAMpos, double *mwf, double *trms );
 //************  2d greyscale viewer
