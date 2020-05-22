8ac5f48291f9c4e6b4f2414cd15aca30376111f8
Florian <florian.hahn@posteo.de>
2020-05-22 17:53:07 +0200

    Change a lot in structure: include only .h, move global definitions to right place

--- CCDExamp/Global.h
+++ CCDExamp/Global.h
@@ -3,22 +3,9 @@
 // can be used for PCIE board
 // for CCDExample and ESLSCDLL
 
-#define _CCDEXAMP
 #define IDM_EXIT           100
-#define DBGNOCAM FALSE	//TRUE if debug with no camera - geht nicht ohne gegenseite: kein clk!
 #define _ERRTEST FALSE //test data for integrity
-#define _33MHz FALSE //true for highest speed cams
-//set TRUE if you want to change the thread priority:
-#define _USETHREAD	TRUE
-//#define _USETHREAD !_FIFO  // with FIFO don't change priority
-#define _THREADPRI 15
-//TRUE if you use PS2 Keyboard, FALSE else
-// on WinNT/2000/xp the function GetAsyncKeystate works only
-// if the Thread Class is not highest or 
-// if the Sleep function is used inside Contimess->Measure
-// so here a special hardware read of PS2Keyboard is used
-// to avoid interrupt dependend routines; look BOARD->WaitTrigger
-#define _PS2KEYBOARD FALSE 
+
 //camera system select
 enum camera_system
 {
@@ -40,39 +27,12 @@ enum adc_mode
 #define GAIN_HIGH FALSE
 //options for 3030
 #define GAIN 6
-//#define _HA_IR FALSE		//  must be true for Ha series G920x IR sensor - not for G11608
-//#define _HA_IRSingleCH FALSE// TRUE for Ha series G920x if IR Sensor has 256 pixel, FALSE if 512 - not G11608
-//#define _IR2 FALSE			// must be true for 2 parallel IR sensors of type G920x - not G11608
-//#define _TI FALSE			// must be true for TI TC253 sensor
-//#define _IS_C4350 FALSE		// must be TRUE for C4350 adapter
-#define _PARALLEL FALSE		//TRUE for 2 Interface Boards
-#define _RESORT FALSE		// resort array for IR G920x - not G11608
-//#define _PPORT FALSE		//TRUE if used on printerport
-//#define _AD16cds FALSE		//TRUE for AD9826 cds 10Mhz
-//#define _AD16ADC FALSE		//TRUE for ADC16061 2Mhz or LTC2204
-#define _AD16ADS TRUE		//TRUE if act AD is used
-#define _HILO FALSE			//FALSE for default
-#define _USESHUTTER FALSE	//TRUE if sensor has shutter function
-//#define _PRGRMVON FALSE		//TRUE for SENDCOMMAND with VON instead of ND
-//#define _OPTSTATE FALSE //decrement pixel if shutterstate input with OPTO1 is used on old boards
 #define _MSHUT FALSE
 #define _MINREPTIME 20
-//ADC16061 specials
-//#define _FFPCI133 FALSE		// PCI version 133.x
-//BOOL _OLDCAL = FALSE;		// TRUE for  ADC16061
 #define _HWCH2	TRUE	//true if 2 words are packet in one long ->resort to db1&db2 = PCI with adaptor board
 						//all double line systems
-#define _RESORTDB	FALSE	//true if ->resort to db1&db2 - only no FIFO
 #define		DRV		1	//1 if only one interface board LSCPCI1 or LSCISA1 in example
 						// could be 2..4 for multiple boards
-// global Camera data
-#define  _NO_TLPS  0x12 //oldpc: 0x11				//0x11=17*128  = 2176 Bytes  = 1088 WORDS
-#define TLPSize 0x20	//0x20 on old pc
-#define RAMPAGESIZE 4096
-#define DMA_BUFSIZEINSCANS 1000		//default: BUFSIZE=1000, INTR all 500
-#define DMA_HW_BUFPARTS 2
-#define DMA_DMASPERINTR DMA_BUFSIZEINSCANS / DMA_HW_BUFPARTS  // alle halben buffer ein intr um hi/lo part zu kopieren deshalb nochmal /2
-#define _PIXEL  1088				// no of pixels min 300, should be multiple of 300, max 8100
 #define CAMCNT 1
 #if CAMCNT == 1
 BOOL DISP2 = FALSE;		//display 1 camera
@@ -92,32 +52,9 @@ BOOL DISP2 = TRUE;		//display 2 cameras parallel, TRUE for double line
 #define INTR_EN TRUE		// enables INTR
 #define MAXPCIECARDS 5
 typedef USHORT ArrayT; //!! USHORT for linear 12/16bit word array or resort or highest speed
-#define MAXVAL USHRT_MAX
 BOOL contimess_run_once = FALSE;
 typedef ArrayT* pArrayT;
 //!! long for standard 
-struct ffloopparams
-{
-	UINT32 board_sel;
-	UINT32 exptus;
-	UINT8 exttrig;
-	UINT8 blocktrigger;
-	UINT8 btrig_ch;
-};
-/*
-//delete after deleting ringreadthread									// array type is defined in board.h
-ArrayT DIODEN[_MAXDB][_PIXEL];		// global data array, could be 1 or 2 dim
-ArrayT DIODENRingBuf[100 * RAMPAGESIZE];
-DWORD FirstPageOffset;
-pArrayT pDIODEN = (pArrayT) &DIODEN;
-
-//jungo
-
-USHORT DMAUserBuf[_PIXEL][USERBUFINSCANS];
-PUSHORT pDMAUserBuf = &DMAUserBuf[0][0];
-//DWORD dwDMABufSize;// = 100 * RAMPAGESIZE * 2;// 100: ringbufsize 2:because  we need the size in bytes
-ULONG DisplData[2][_PIXEL];//array for display for 2 cams parallel
-*/
 //this is the read from FIFO frequency
 static BOOL SYM_PULSE = FALSE;		// read FIFO is allways higest speed
 static BOOL BURSTMODE = TRUE;
@@ -127,7 +64,6 @@ static 	unsigned long WAITS = 0x0;
 //this is the write to FIFO frequency / FREQ=datarate=pclk*2 if 12/16bit
 static ULONG FREQ = 0; //=waits -> 0=33MHz;1=16MHz;2=8MHz max 6=0 
 static ULONG DELAYini = 0;			//DELAY for WRFIFO
-static ULONG XCKDELAY = 3; //100ns+n*400ns, 1<n<8 Sony=7
 //only for ISA
 static	unsigned long FOURMAX = _MAXDB * _PIXEL;
 #define PPORTADR 0 //0x378	//PrinterPortaddress or 0 if ISA Board
@@ -171,8 +107,6 @@ BOOL EXTTRIGFLAG = FALSE;		// run with external Trigger
 int TrigMod = 0;						//pos slope
 //static BOOL HISLOPE = TRUE;			// Slope for external Trigger
 static BOOL HIAMP = FALSE;			// Amplification for switchable sensors
-__int64 TPS = 0;				// ticks per second; is set in InitHRCounter
-__int64 START = 0;				// global variable for sync to systemtimer
 //__int64 DELAY = 0;			   //also set in InitHRCounter
 __int64 TICKSDISP = 0;			//display time in ticks
 // Display data
@@ -197,7 +131,6 @@ int TempLevel = 0;
 #define _COOLER FALSE
 //for 2 thread display
 BOOL GetNextScan = FALSE;
-BOOL Running = FALSE;
 BOOL UpdateDispl = FALSE;
 //for trms calcs
 #define TRMSpix  1 //(8-1)*150-100// _PIXEL/2	//pixel no for which the rms value is sampled
@@ -231,15 +164,12 @@ HANDLE hCopyToDispBuf;//Mutex for data buffer write
 //#define DMABufSizeInScans  1000
 //globals
 WORD UserBufInScans;
-USHORT* pDMABigBufIndex[3] = { NULL, NULL, NULL };
 //INT_PTR pDMABigBufBase = NULL;
 //jungo
-pArrayT pBLOCKBUF[3] = { NULL, NULL, NULL };
 //USHORT DMAUserBuf[1200][USERBUFINSCANS];//not for dll
 //WORD UserBufInScans = USERBUFINSCANS;
 //DMAUserBuf is the complete memory of the application
 //PUSHORT pDMABigBuf; //not for dll
-PUSHORT pDMABigBufBase[3]; //not for dll
 //DWORD dwDMABufSize;// = 100 * RAMPAGESIZE * 2;// 100: ringbufsize 2:because  we need the size in bytes
 ULONG DisplData[2][1200 * CAMCNT];//array for display for 2 cams parallel
 //delete after deleting ringreadthread									// array type is defined in board.h
@@ -247,54 +177,11 @@ ULONG DisplData[2][1200 * CAMCNT];//array for display for 2 cams parallel
 //ArrayT DIODENRingBuf[(DMABufSizeInScans + 10) * 1200 * sizeof(USHORT)];
 DWORD FirstPageOffset;
 //pArrayT pDIODEN = (pArrayT)&DIODEN;
-int Nob = 1;
 #if _MSHUT == TRUE
-int Nospb = 100;
+//Nospb = 100;
 #else
-int Nospb = 1000;
+//Nospb = 1000;
 #endif
 #define _FORCETOPLS128 TRUE	//only use payload size 128byte
 ULONG NO_TLPS = 0x12; //was 0x11-> x-offset			//0x11=17*128  = 2176 Bytes  = 1088 WORDS
 ULONG TLPSIZE = 0x20;
-
-// Prototypes 
-LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
-LRESULT CALLBACK About(HWND, UINT, WPARAM, LPARAM);
-LRESULT CALLBACK SetupMeasure(HWND hDlg,
-	UINT message,
-	WPARAM wParam,
-	LPARAM lParam);
-
-LRESULT CALLBACK SetupTLevel(HWND hDlg,
-	UINT message,
-	WPARAM wParam,
-	LPARAM lParam);
-
-LRESULT CALLBACK SetupEC(HWND hDlg,
-	UINT message,
-	WPARAM wParam,
-	LPARAM lParam);
-LRESULT CALLBACK AllocateBuf(HWND hDlg,
-	UINT message,
-	WPARAM wParam,
-	LPARAM lParam);
-
-LRESULT CALLBACK ChooseBoard(HWND hDlg,
-	UINT message,
-	WPARAM wParam,
-	LPARAM lParam);
-
-LRESULT CALLBACK Set3ROI(HWND hDlg,
-	UINT message,
-	WPARAM wParam,
-	LPARAM lParam);
-
-LRESULT CALLBACK Set5ROI(HWND hDlg,
-	UINT message,
-	WPARAM wParam,
-	LPARAM lParam);
-
-LRESULT CALLBACK FullBinning(HWND hDlg,
-	UINT message,
-	WPARAM wParam,
-	LPARAM lParam);
