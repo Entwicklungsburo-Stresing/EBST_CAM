ec3244dc1e18cf870043c43769e6064286c8d613
Florian <florian.hahn@posteo.de>
2020-06-19 12:18:16 +0200

    Fix some global vars in board

--- shared_src/Board.h
+++ shared_src/Board.h
@@ -69,6 +69,12 @@ struct ffloopparams
 	UINT8 btrig_ch;
 };
 
+struct global_vars
+{
+	WDC_DEVICE_HANDLE* hDev;
+	USHORT** pDMABigBufBase;
+};
+
 typedef USHORT ArrayT; //!! USHORT for linear 12/16bit word array or resort or highest speed
 typedef ArrayT* pArrayT;
 
@@ -82,7 +88,6 @@ extern USHORT** pDMABigBufBase;
 extern int Nob;
 extern int Nospb;
 extern ULONG aCAMCNT[5];	// cameras parallel
-extern WDC_DEVICE_HANDLE hDev[MAXPCIECARDS];
 extern WDC_DEVICE_HANDLE* hDev;
 extern BOOL escape_readffloop;
 extern BOOL contffloop;
@@ -93,7 +98,7 @@ extern BOOL Running;
 extern pArrayT pBLOCKBUF[3];
 extern UINT32 BOARD_SEL;
 
-
+static ULONG XCKDELAY = 3; //100ns+n*400ns, 1<n<8 Sony=7
 
 void ErrMsgBoxOn( void );
 void ErrMsgBoxOff( void ); // switch to suppress error message boxes
