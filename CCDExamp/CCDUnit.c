9ba1921fdc3cfd1f836bac8acfb3a0b914a4b561
Florian Hahn <florian.hahn@posteo.de>
2021-01-14 14:07:28 +0100

    Add TRMS calculation for second camera

--- CCDExamp/CCDUnit.c
+++ CCDExamp/CCDUnit.c
@@ -342,7 +342,8 @@ void UpdateTxT(void)
 	{
 		//j=sprintf(TrmsString,"                                                           ") ;//clear old display
 		//TextOut(hMSDC,20,YLENGTH + 50,TrmsString,j);
-		j += sprintf_s(TrmsString + j, 260, " Trms of Pixel %lu CH1 is %.1f ", TRMSpix, TRMSval_global[0]);
+		j += sprintf_s( TrmsString + j, 260, " Trms of Pixel %lu CH1 is %.1f ", TRMSpix, TRMSval_global[0] );
+		if (CAMCNT > 1) j += sprintf_s( TrmsString + j, 260, ", Trms of Pixel %lu CH2 is %.1f ", TRMSpix, TRMSval_global[1] );
 		j += sprintf_s(TrmsString + j, 260, " -- scan: %lu, err: %lu         ", DisplData[0][5],ERRCNT);
 		TextOut(hMSDC, 20, YLENGTH + 50, TrmsString, j);
 	}
