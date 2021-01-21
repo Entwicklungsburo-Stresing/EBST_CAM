d23bd498532f31388b0cf571432d84ad0bcee20d
Florian Hahn <florian.hahn@posteo.de>
2021-01-21 13:46:23 +0100

    Cleanup CCDUnit. Move CalcTRMS to notifyMeasureDone

--- CCDExamp/UIAbstractionLayer.c
+++ CCDExamp/UIAbstractionLayer.c
@@ -1,26 +1,28 @@
 //UI abstraction layer for CCDExample
 #include "shared_src/UIAbstractionLayer.h"
+#include "CCDExamp.h"
 
-void notifyMeasureStart()
+void notifyMeasureStart( UINT32 drv )
 {
 
 	return;
 }
 
-void notifyMeasureDone()
+void notifyMeasureDone( UINT32 drv )
+{
+	double mwf = 0.0; //unused
+	CalcTrms( DRV, *Nospb, TRMSpix, 0, &mwf, &TRMSval_global[0] );
+	if (CAMCNT > 1) CalcTrms( DRV, *Nospb, TRMSpix, 1, &mwf, &TRMSval_global[1] );
+	return;
+}
+
+void notifyBlockStart( UINT32 drv )
 {
 
 	return;
 }
 
-void notifyBlockStart()
+void notifyBlockDone( UINT32 drv )
 {
-
-	return;
-}
-
-void notifyBlockDone()
-{
-
 	return;
 }
