a2036393e079d60dc8341b34914cdfb60abb9094
Gerhard Stresing <info@stresing.de>
2021-01-25 11:57:10 +0100

    update

--- CCDExamp/Global.h
+++ CCDExamp/Global.h
@@ -34,22 +34,23 @@ enum adc_mode
 #define	DRV	1	//1 if only one interface board LSCPCI1 or LSCISA1 in example
 						// could be 2..4 for multiple boards
 #define CAMCNT 2
-#define _PIXEL 1088
+#define _PIXEL 1088		//1088 - 576 - 320 - 192
 //settings for 8 Bit cameras
 //static	unsigned long FLAG816 = 2;  // 2=8Bit, 1=12/16Bit
 //static int YSHIFT = 0;				// 8Bit=0, 12Bit=4 or more for ddrep>1
-static int YSHIFT = 4;				// 12Bit=4 or more for addrep>1
-//static int YSHIFT = 8;			// 16bit=8
+//static int YSHIFT = 4;				// 12Bit=4 or more for addrep>1
+static int YSHIFT = 8;			// 16bit=8
 //static int YSHIFT = 6;			// 14 bit
 // camera values for calling GETCCD and InitBoard
-#define _FFTLINES 64 		// no of vertical lines of FFT sensors, usually 64
-							// =0 if not FFT
+
 enum sensor_type
 {
 	PDAsensor = 0,		//set RS after read; for HA S39xx
 	FFTsensor = 1,		//set vclk generator; for HA S703x
 };
 #define SENSOR_TYPE FFTsensor
+#define _FFTLINES 64 		// no of vertical lines of FFT sensors, usually 64
+// =0 if not FFT
 #define Vfreqini 7		//vclk freq for FFTs with FIFO in divider of12MHz (0..15)
 						//=4 for highest speed with 7030-0906
 enum trigger_mode
