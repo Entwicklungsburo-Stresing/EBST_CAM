3729a27d4e3cd685c8228aef0b790f836555a5ff
Florian Hahn <florian.hahn@posteo.de>
2021-02-03 19:27:12 +0100

    Add settings

--- lsc-gui.cpp
+++ lsc-gui.cpp
@@ -4,6 +4,9 @@
 int main(int argc, char *argv[])
 {
   QApplication app(argc, argv);
+  QCoreApplication::setOrganizationName("Entwicklungsbuero Stresing");
+  QCoreApplication::setOrganizationDomain("stresing.de");
+  QCoreApplication::setApplicationName("LSC");
   MainWindow w;
   w.show();
   return app.exec();
