5df4a8c0b1707bbac5443ddeb4514e6eb6db474c
Florian Hahn <florian.hahn@posteo.de>
2021-03-16 16:12:58 +0100

    Add all setting paths and default values

--- lsc-gui/dialogsettings.cpp
+++ lsc-gui/dialogsettings.cpp
@@ -8,8 +8,8 @@ DialogSettings::DialogSettings(QSettings* settings, QWidget *parent) :
     ui->setupUi(this);
     connect(ui->buttonBox, SIGNAL(accepted()), this, SLOT(on_accepted()));
     _settings = settings;
-    ui->comboBoxTheme->setCurrentIndex(_settings->value(SETTING_THEME, THEME_DEFAULT).toInt());
-    ui->comboBoxOutput->setCurrentIndex(_settings->value(SETTING_TOR, TOR_DEFAULT).toInt());
+    ui->comboBoxTheme->setCurrentIndex(_settings->value(settingThemePath, settingThemeDefault).toInt());
+    ui->comboBoxOutput->setCurrentIndex(_settings->value(settingTorPath, settingTorDefault).toInt());
 }
 
 DialogSettings::~DialogSettings()
@@ -19,12 +19,12 @@ DialogSettings::~DialogSettings()
 
 void DialogSettings::on_accepted()
 {
-    _settings->setValue(SETTING_NOS, ui->spinBoxNos->value());
-    _settings->setValue(SETTING_NOB, ui->spinBoxNob->value());
-    _settings->setValue(SETTING_CAMCNT, ui->spinBoxCamcnt->value());
-    _settings->setValue(SETTING_THEME, ui->comboBoxTheme->currentIndex());
-    _settings->setValue(SETTING_TOR, ui->comboBoxOutput->currentIndex());
-    _settings->setValue(SETTING_PIXEL, ui->spinBoxPixel->value());
+    _settings->setValue(settingNosPath, ui->spinBoxNos->value());
+    _settings->setValue(settingNobPath, ui->spinBoxNob->value());
+    _settings->setValue(settingCamcntPath, ui->spinBoxCamcnt->value());
+    _settings->setValue(settingThemePath, ui->comboBoxTheme->currentIndex());
+    _settings->setValue(settingTorPath, ui->comboBoxOutput->currentIndex());
+    _settings->setValue(settingPixelPath, ui->spinBoxPixel->value());
     emit settings_saved();
     return;
 }
