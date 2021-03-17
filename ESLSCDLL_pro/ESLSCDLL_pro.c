cd7404d74d14fa544a348262db2e3cd816cca36a
Gerhard Stresing <info@stresing.de>
2021-03-17 10:47:39 +0100

    added comments

--- ESLSCDLL_pro/ESLSCDLL_pro.c
+++ ESLSCDLL_pro/ESLSCDLL_pro.c
@@ -1,20 +1,19 @@
 /*
+/*
 This file is part of ESLSCDLL.
 
-ESLSCDLL is free software : you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+ESLSCDLL_pro is not free software :
+all Code is under
+
+Copyright 2021 ©Entwicklungsbuero G. Stresing (http://www.stresing.de/)
+
+you can use it as is, but
+for further sale ask Entwicklungsbuero G. Stresing for permission
 
 ESLSCDLL is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
-GNU General Public License for more details.
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 
-You should have received a copy of the GNU General Public License
-along with Foobar.If not, see < http://www.gnu.org/licenses/>.
-
-Copyright 2020 Entwicklungsbuero G. Stresing (http://www.stresing.de/)
 */
 
 #include "shared_src/ESLSCDLL_pro.h"
