9044cf09cde24920ee2247885879594cadee5306
Florian Hahn <florian.hahn@posteo.de>
2021-05-12 13:12:29 +0200

    Implement initMeasurement in crossPlattformBoard

--- shared_src/crossPlattformBoard_ll.h
+++ shared_src/crossPlattformBoard_ll.h
@@ -1,5 +1,7 @@
+#ifndef CROSSPLATTFORMBOARDLL_H
+#define CROSSPLATTFORMBOARDLL_H
+
 #include <stdint.h>
-#include <stdio.h>
 #include "es_status_codes.h"
 
 #ifdef WIN32
@@ -7,8 +9,22 @@
 #endif
 
 #ifdef __linux__
+#include <stdio.h>
 #define ES_LOG(...) fprintf(stderr, __VA_ARGS__);
 #endif
 
-es_status_codes setBitS0(uint32_t drvno, uint32_t bitnumber, uint16_t address);
-es_status_codes resetBitS0(uint32_t drvno, uint32_t bitnumber, uint16_t address);
+// Low level API
+// platform specific implementation
+
+es_status_codes checkDriverHandle(uint32_t drvno);
+es_status_codes readRegister_32( uint32_t drvno, uint32_t* data, uint16_t address );
+es_status_codes readRegister_16( uint32_t drvno, uint16_t* data, uint16_t address );
+es_status_codes readRegister_8( uint32_t drvno, uint8_t* data, uint16_t address );
+es_status_codes writeRegister_32( uint32_t drvno, uint32_t data, uint16_t address );
+es_status_codes writeRegister_16( uint32_t drvno, uint16_t data, uint16_t address );
+es_status_codes writeRegister_8( uint32_t drvno, uint8_t data, uint16_t address );
+void FreeMemInfo( uint64_t *pmemory_all, uint64_t *pmemory_free );
+es_status_codes SetupPCIE_DMA( uint32_t drvno );
+es_status_codes SetTLPS( uint32_t drvno, uint32_t pixel );
+
+#endif // CROSSPLATTFORMBOARDLL_H
