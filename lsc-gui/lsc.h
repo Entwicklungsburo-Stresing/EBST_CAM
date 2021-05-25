af0d65ffe8601dd6da1d97790d5cba707b2b4347
Florian Hahn <florian.hahn@posteo.de>
2021-05-25 13:41:41 +0200

    Make lsc.cpp plattform independent

--- lsc-gui/lsc.h
+++ lsc-gui/lsc.h
@@ -15,15 +15,15 @@ public:
     Lsc();
     ~Lsc();
     es_status_codes initDriver();
-    es_status_codes initPcieBoard();
+    es_status_codes initPcieBoard(uint32_t drvno);
 	es_status_codes initMeasurement(struct global_settings settings_struct);
     es_status_codes returnFrame(uint32_t board, uint32_t sample, uint32_t block, uint16_t camera, uint16_t *pdest, uint32_t length);
-    es_status_codes _abortMeasurement();
+    es_status_codes _abortMeasurement(uint32_t drvno);
     std::string driverInstructions;
-    std::string dumpS0Registers();
-    std::string dumpDmaRegisters();
-    std::string dumpTlp();
-    void setTorOut(uint8_t torOut);
+    std::string dumpS0Registers(uint32_t drvno);
+    std::string dumpDmaRegisters(uint32_t drvno);
+    std::string dumpTlp(uint32_t drvno);
+    void setTorOut(uint32_t drvno, uint8_t torOut);
 public slots:
     es_status_codes startMeasurement();
 signals:
