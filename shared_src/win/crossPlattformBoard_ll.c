dcc38e8daf0ab4d4a76b686ad6931c6c29f53c58
Florian Hahn <florian.hahn@posteo.de>
2021-05-27 17:34:43 +0200

    Add InitProDLL, CleanupDma, LargeToInt, InitHRCounter

--- shared_src/win/crossPlattformBoard_ll.c
+++ shared_src/win/crossPlattformBoard_ll.c
@@ -6,11 +6,107 @@
 #include "Jungo/windrvr.h"
 #include "Jungo/wdc_lib.h"
 #include "Jungo/wdc_defs.h"
+#include "shared_src/lscpciej_lib.h"
+#include "shared_src/ESLSCDLL_pro.h"
 
 WDC_DEVICE_HANDLE hDev_tmp[MAXPCIECARDS];
 WDC_DEVICE_HANDLE* hDev = &hDev_tmp;
 DWORD dmaBufferSizeInBytes = 0;
 uint16_t* dmaBuffer[MAXPCIECARDS] = { NULL, NULL, NULL, NULL, NULL };
+DWORD64 IsrCounter = 0;
+UINT8 dmaBufferPartReadPos[MAXPCIECARDS] = { 0, 0, 0, 0, 0 };
+WD_DMA* dmaBufferInfos[MAXPCIECARDS] = { NULL, NULL, NULL, NULL, NULL }; //there will be saved the neccesary parameters for the dma buffer
+UINT16* userBufferWritePos[MAXPCIECARDS] = { NULL, NULL, NULL, NULL, NULL };
+WDC_PCI_SCAN_RESULT scanResult;
+WD_PCI_CARD_INFO deviceInfo[MAXPCIECARDS];
+__int64 TPS = 0;
+
+struct global_vars
+{
+	USHORT** userBuffer;
+	WDC_DEVICE_HANDLE* hDev;
+	//PWDC_DEVICE* pDev;
+	ULONG* aPIXEL;
+	ULONG* aCAMCNT;
+	UINT32* Nospb;
+	BOOL* useSWTrig;
+};
+
+/**
+\brief Initializes the pro DLL. Call this before using it. While initialization global variables are set in pro dll.
+\return void
+*/
+void InitProDLL()
+{
+	struct global_vars g;
+	g.userBuffer = userBuffer;
+	g.hDev = hDev;
+	g.aPIXEL = aPIXEL;
+	g.aCAMCNT = aCAMCNT;
+	g.Nospb = Nospb;
+	g.useSWTrig = useSWTrig;
+	DLLInitGlobals(g);
+	return;
+}
+
+/**
+ * \brief
+ *
+ * \param drvno PCIe board identifier.
+ * \return es_status_codes:
+ *		- es_no_error
+ *		- es_unlocking_dma_failed
+ */
+es_status_codes CleanupDma(uint32_t drvno)
+{
+	ES_LOG("Cleanup DMA\n");
+	/* Disable DMA interrupts */
+	WDC_IntDisable(hDev[drvno]);
+	/* Unlock and free the DMA buffer */
+	DWORD dwStatus = WDC_DMABufUnlock(dmaBufferInfos[drvno]);
+	if (WD_STATUS_SUCCESS != dwStatus)
+	{
+		ErrLog("Failed unlocking a contiguous DMA buffer. Error 0x%lx - %s\n", dwStatus, Stat2Str(dwStatus));
+		WDC_Err("%s", LSCPCIEJ_GetLastErr());
+		return es_unlocking_dma_failed;
+	}
+	WDC_Err("Unlock DMABuf Successfull\n");
+	return es_no_error;
+}
+
+// System Timer in Ticks
+/**
+\brief Converts Large to Int64.
+*/
+UINT64 LargeToInt(LARGE_INTEGER li)
+{
+	UINT64 res = 0;
+	res = li.HighPart;
+	res = res << 32;
+	res = res + li.LowPart;
+	return res;
+} //LargeToInt
+
+/**
+\brief Init high resolution counter.
+\return TPS ticks per sec
+*/
+int64_t InitHRCounter()
+{
+	BOOL ifcounter;
+	UINT64 tps = 0;
+	LARGE_INTEGER freq;
+	freq.LowPart = 0;
+	freq.HighPart = 2;
+
+	//tps:: ticks per second = freq
+	ifcounter = QueryPerformanceFrequency(&freq);
+	tps = LargeToInt(freq); //ticks per second
+	WDC_Err("TPS: %lld\n", tps);
+
+	return tps;
+}
+
 
 /**
 \brief This call comes every DMASPERINTR=500 here a DMASubBuf could be copied to the DMABigBuf.
@@ -20,7 +116,7 @@ The INTR occurs every DMASPERINTR and copies this block of scans in lower/upper 
 void isr( uint32_t drvno, void* pData )
 {
 	WDC_Err( "*isr(): 0x%x\n", IsrCounter );
-	es_status_codes status = SetS0Bit( IRQFLAGS_bitindex_INTRSR, S0Addr_IRQREG, drvno );//set INTRSR flag for TRIGO
+	es_status_codes status = setBitS0_32(drvno, IRQFLAGS_bitindex_INTRSR, S0Addr_IRQREG );//set INTRSR flag for TRIGO
 	if (status != es_no_error) return;
 	//! be sure not to stop run before last isr is ready - or last part is truncated
 	//usually dmaBufferSizeInBytes = 1000scans 
@@ -33,7 +129,7 @@ void isr( uint32_t drvno, void* pData )
 	{
 		WDC_Err( "numberOfInterrupts: 0x%x \n", numberOfInterrupts );
 		WDC_Err( "ISR Counter overflow: 0x%x \n", IsrCounter );
-		status = ResetS0Bit( IRQFLAGS_bitindex_INTRSR, S0Addr_IRQREG, drvno );//reset INTRSR flag for TRIGO
+		status = resetBitS0_32( drvno, IRQFLAGS_bitindex_INTRSR, S0Addr_IRQREG );//reset INTRSR flag for TRIGO
 		return;
 	}
 	WDC_Err("dmaBufferSizeInBytes: 0x%x \n", dmaBufferSizeInBytes);
@@ -46,7 +142,7 @@ void isr( uint32_t drvno, void* pData )
 	if (dmaBufferPartReadPos[drvno] >= DMA_BUFFER_PARTS)		//number of ISR per dmaBuf - 1
 		dmaBufferPartReadPos[drvno] = 0;						//dmaBufferPartReadPos is 0 or 1 for buffer devided in 2 parts
 	userBufferWritePos[drvno] += dmaBufferPartSizeInBytes / sizeof( UINT16 );
-	status = ResetS0Bit( IRQFLAGS_bitindex_INTRSR, S0Addr_IRQREG, drvno );//reset INTRSR flag for TRIGO
+	status = resetBitS0_32(drvno, IRQFLAGS_bitindex_INTRSR, S0Addr_IRQREG );//reset INTRSR flag for TRIGO
 	IsrCounter++;
 	return;
 }
@@ -69,7 +165,29 @@ es_status_codes readRegister_32(uint32_t drvno, uint32_t* data, uint16_t address
 	volatile DWORD dwStatus = WDC_ReadAddrBlock(hDev[drvno], 0, address, sizeof(uint32_t), data, WDC_MODE_8, WDC_ADDR_RW_DEFAULT);
 	if (WD_STATUS_SUCCESS != dwStatus)
 	{
-		ES_LOG("ReadLongDMA in address 0x%x failed\n", address);
+		ES_LOG("readRegister_32 in address 0x%x failed\n", address);
+		ES_LOG("%s", LSCPCIEJ_GetLastErr());
+		return es_register_read_failed;
+	}
+	return es_no_error;
+};
+
+/**
+ * @brief Reads long on DMA area.
+ *
+ * @param drvno PCIe board identifier
+ * @param data buffer for data
+ * @param address Offset from BaseAdress - in Bytes ! 0..3= Regs of Board.
+ * @return es_status_codes
+	- es_no_error
+	- es_register_read_failed
+ */
+es_status_codes readRegister_16(uint32_t drvno, uint16_t* data, uint16_t address)
+{
+	volatile DWORD dwStatus = WDC_ReadAddrBlock(hDev[drvno], 0, address, sizeof(uint16_t), data, WDC_MODE_8, WDC_ADDR_RW_DEFAULT);
+	if (WD_STATUS_SUCCESS != dwStatus)
+	{
+		ES_LOG("readRegister_16 in address 0x%x failed\n", address);
 		ES_LOG("%s", LSCPCIEJ_GetLastErr());
 		return es_register_read_failed;
 	}
@@ -92,14 +210,14 @@ es_status_codes readRegister_8(uint32_t drvno, uint8_t* data, uint16_t address)
 	//the second parameter gives the memory space 0:mem mapped cfg/S0-space 1:I/O cfg/S0-space 2:DMA-space
 	if (WD_STATUS_SUCCESS != dwStatus)
 	{
-		ES_LOG("ReadByteS0 in address 0x%x failed\n", address);
+		ES_LOG("readRegister_8 in address 0x%x failed\n", address);
 		return es_register_read_failed;
 	}
 	return es_no_error;
 };
 
 /**
- * \brief Writes long to space0 register.
+ * \brief Writes 32 bits (4 bytes) to register.
  *
  * \param drvno PCIe board identifier.
  * \param data data to write
@@ -114,13 +232,35 @@ es_status_codes writeRegister_32(uint32_t drvno, uint32_t data, uint16_t address
 	//the second parameter gives the memory space 0:mem mapped cfg/S0-space 1:I/O cfg/S0-space 2:DMA-space
 	if (WD_STATUS_SUCCESS != dwStatus)
 	{
-		ES_LOG("WriteLongDMA in address 0x%x with data: 0x%x failed\n", address, data);
+		ES_LOG("writeRegister_32 in address 0x%x with data: 0x%x failed\n", address, data);
 		ES_LOG("%s", LSCPCIEJ_GetLastErr());
 		return es_register_write_failed;
 	}//else WDC_Err("DMAWrite /t address /t0x%x /t data: /t0x%x \n", address, data);
 	return es_no_error;
 };
 
+/**
+ * \brief Writes 16 bits (2 bytes) to register.
+ *
+ * \param drvno PCIe board identifier.
+ * \param data data to write
+ * \param address Register offset from BaseAdress - in bytes
+ * \return es_status_codes
+	- es_no_error
+	- es_register_write_failed
+ */
+es_status_codes writeRegister_16(uint32_t drvno, uint16_t data, uint16_t address)
+{
+	DWORD dwStatus = WDC_WriteAddrBlock(hDev[drvno], 0, address, sizeof(uint16_t), &data, WDC_MODE_8, WDC_ADDR_RW_DEFAULT);
+	//the second parameter gives the memory space 0:mem mapped cfg/S0-space 1:I/O cfg/S0-space 2:DMA-space
+	if (WD_STATUS_SUCCESS != dwStatus)
+	{
+		ES_LOG("writeRegister_16 in address 0x%x with data: 0x%x failed\n", address, data);
+		ES_LOG("%s", LSCPCIEJ_GetLastErr());
+		return es_register_write_failed;
+	}//else WDC_Err("DMAWrite /t address /t0x%x /t data: /t0x%x \n", address, data);
+	return es_no_error;
+};
 
 /**
 * \brief Write byte (8 bit) to register in space0 of PCIe board, except r10-r1f.
@@ -138,7 +278,7 @@ es_status_codes writeRegister_8(uint32_t drv, uint8_t data, uint16_t address)
 	//the second parameter gives the memory space 0:mem mapped cfg/S0-space 1:I/O cfg/S0-space 2:DMA-space
 	if (WD_STATUS_SUCCESS != dwStatus)
 	{
-		ES_LOG("WriteByteS0 in address 0x%x with data: 0x%x failed\n", address, data);
+		ES_LOG("writeRegister_8 in address 0x%x with data: 0x%x failed\n", address, data);
 		return es_register_write_failed;
 	}//else WDC_Err("ByteS0Write /t address /t0x%x /t data: /t0x%x \n", address, data);
 	return es_no_error;
@@ -191,8 +331,7 @@ es_status_codes SetupDma( uint32_t drvno )
 	//If interrupt was enabled before, first cleanup DMA.
 	if (WDC_IntIsEnabled(hDev[drvno]))
 	{
-		ES_LOG("Cleanup DMA\n");
-		es_status_codes status = CleanupPCIE_DMA(drvno);
+		es_status_codes status = CleanupDma(drvno);
 		if (status != es_no_error) return status;
 	}
 	dmaBufferSizeInBytes = DMA_BUFFER_SIZE_IN_SCANS * aPIXEL[drvno] * sizeof( UINT16 );
@@ -225,22 +364,25 @@ es_status_codes enableInterrupt( uint32_t drvno )
 	switch (drvno)
 	{
 	case 1:
-		dwStatus = LSCPCIEJ_IntEnable( hDev[drvno], interrupt_handler1 );
+	{
+		DWORD dwStatus = LSCPCIEJ_IntEnable(hDev[drvno], interrupt_handler1);
 		if (WD_STATUS_SUCCESS != dwStatus)
 		{
-			ES_LOG( "Failed to enable the Interrupts1. Error 0x%lx - %s\n",	dwStatus, Stat2Str( dwStatus ) );
+			ES_LOG("Failed to enable the Interrupts1. Error 0x%lx - %s\n", dwStatus, Stat2Str(dwStatus));
 			return es_enabling_interrupts_failed;
 		}
 		break;
-
+	}
 	case 2:
-		dwStatus = LSCPCIEJ_IntEnable( hDev[drvno], interrupt_handler2 );
+	{
+		DWORD dwStatus = LSCPCIEJ_IntEnable(hDev[drvno], interrupt_handler2);
 		if (WD_STATUS_SUCCESS != dwStatus)
 		{
-			ES_LOG( "Failed to enable the Interrupts2. Error 0x%lx - %s\n", dwStatus, Stat2Str( dwStatus ) );
+			ES_LOG("Failed to enable the Interrupts2. Error 0x%lx - %s\n", dwStatus, Stat2Str(dwStatus));
 			return es_enabling_interrupts_failed;
 		}
 		break;
+	}
 	default:
 		return es_parameter_out_of_range;
 	}
@@ -257,7 +399,7 @@ void ResetBufferWritePos(uint32_t drvno)
     return;
 }
 
-void copyRestData(size_t rest_in_bytes)
+void copyRestData(uint32_t drvno, size_t rest_in_bytes)
 {
 	ES_LOG( "Copy rest data:\n" );
 	ES_LOG( "dmaBufferSizeInBytes: 0x%x \n", dmaBufferSizeInBytes );
@@ -270,8 +412,7 @@ void copyRestData(size_t rest_in_bytes)
 es_status_codes _InitBoard(uint32_t drvno)
 {
 	if ((drvno < 1) || (drvno > 2)) return es_invalid_driver_number;
-	//PWDC_DEVICE pDev = (PWDC_DEVICE)hDev;
-	volatile DWORD dwStatus = 0;
+	DWORD dwStatus = 0;
 	ES_LOG( "Info: scan result: a board found:%lx , dev=%lx, ven=%lx \n", scanResult.dwNumDevices, scanResult.deviceId[drvno - 1].dwDeviceId, scanResult.deviceId[drvno - 1].dwVendorId );
 	//gives the information received from PciScanDevices to PciGetDeviceInfo
 	BZERO( deviceInfo[drvno] );
@@ -299,7 +440,9 @@ es_status_codes _InitBoard(uint32_t drvno)
 	}
 	PWDC_DEVICE pDev = ((PWDC_DEVICE)hDev[drvno]);
 	ES_LOG( "DRVInit hDev id % x, hDev pci slot %x, hDev pci bus %x, hDev pci function %x, hDevNumAddrSp %x \n"	, pDev->id, pDev->slot.dwSlot, pDev->slot.dwBus, pDev->slot.dwFunction, pDev->dwNumAddrSpaces );
+#ifdef WIN32
 	InitProDLL();
+#endif
 	return es_no_error ;
 }
 
@@ -349,7 +492,6 @@ es_status_codes _InitDriver()
 			dwStatus, Stat2Str( dwStatus ) );
 		//doesnt work at this moment before debugsetup
 		WDC_Err( "%s", LSCPCIEJ_GetLastErr() );
-		ErrorMsg( "Failed to initialize the WDC library. Maybe the driver was not unloaded correctly.\n" );
 		WDC_DriverClose();
 		return es_driver_init_failed;
 	}
@@ -361,9 +503,7 @@ es_status_codes _InitDriver()
 		ErrLog( "DeviceFind: Failed scanning the PCI bus.\n"
 			"Error: 0x%lx - %s\n", dwStatus, Stat2Str( dwStatus ) );
 		WDC_Err( "%s", LSCPCIEJ_GetLastErr() );
-		ErrorMsg( "Device not found" );
 		WDC_DriverClose();
-		ErrorMsg( "driver closed.\n" );
 		return es_device_not_found;
 	}
 	number_of_boards = (UINT8) scanResult.dwNumDevices;
@@ -379,8 +519,7 @@ es_status_codes _ExitDriver(uint32_t drvno)
 	ES_LOG( "Driver exit, drv: %u\n", drvno);
 	if (WDC_IntIsEnabled( hDev[drvno] ))
 	{
-		ES_LOG( "cleanup dma\n" );
-		status = CleanupPCIE_DMA( drvno );
+		status = CleanupDma( drvno );
 		if (status != es_no_error) return status;
 	}
 	WDC_DriverClose();
@@ -431,9 +570,70 @@ es_status_codes readConfig_32( uint32_t drvno, uint32_t* data, uint16_t address 
 	if (WD_STATUS_SUCCESS != dwStatus)
 	{
 		WDC_Err( "ReadLongIOPort in address 0x%x failed\n", address );
-		//old message...i kept it because i dont know what it does
-		ErrorMsg( "Read IORunReg failed" );
 		return es_register_read_failed;
 	}
 	return es_no_error;
+}
+
+/**
+ * \brief Write long (32 bit) to register in space0 of PCIe board.
+ *
+ * \param drvno board number (=1 if one PCI board)
+ * \param data long value to write
+ * \param address offset from base address of register (count in bytes)
+ * \return es_status_codes:
+ *		- es_no_error
+ *		- es_register_write_failed
+ */
+es_status_codes writeConfig_32(uint32_t drvno, uint32_t data, uint16_t address)
+{
+	volatile DWORD dwStatus = 0;
+	dwStatus = WDC_PciWriteCfg(hDev[drvno], address, &data, sizeof(uint32_t));
+	if (WD_STATUS_SUCCESS != dwStatus)
+	{
+		WDC_Err("writeConfig_32 in address 0x%x with data: 0x%x failed\n", address, data);
+		return es_register_write_failed;
+	}//else WDC_Err("I0PortWrite /t address /t0x%x /t data: /t0x%x \n", address, DWData);
+	return es_no_error;
+};
+
+/**
+\brief Get the free and installed memory info.
+\param pmemory_all how much is installed
+\param pmemory_free how much is free
+\return none
+*/
+void FreeMemInfo(uint64_t* pmemory_all, uint64_t* pmemory_free)
+{
+	// Use to convert bytes to KB
+#define DIV 1024
+	// Specify the width of the field in which to print the numbers. 
+	// The asterisk in the format specifier "%*I64d" takes an integer 
+	// argument and uses it to pad and right justify the number.
+#define WIDTH 7
+	MEMORYSTATUSEX statex;
+	statex.dwLength = sizeof(statex);
+	GlobalMemoryStatusEx(&statex);
+	//_tprintf(TEXT("There is  %*ld percent of memory in use.\n"),
+	//	WIDTH, statex.dwMemoryLoad);
+	//_tprintf(TEXT("There are %*I64d total KB of physical memory.\n"),
+	//	WIDTH, 
+	*pmemory_all = statex.ullTotalPhys;
+	// DIV);
+	//_tprintf(TEXT("There are %*I64d free  KB of physical memory.\n"),
+	//WIDTH, 
+	*pmemory_free = statex.ullAvailPhys;
+	// DIV);
+	//_tprintf(TEXT("There are %*I64d total KB of paging file.\n"),
+	//	WIDTH, statex.ullTotalPageFile / DIV);
+	//_tprintf(TEXT("There are %*I64d free  KB of paging file.\n"),
+	//	WIDTH, statex.ullAvailPageFile / DIV);
+	//_tprintf(TEXT("There are %*I64d total KB of virtual memory.\n"),
+	//	WIDTH, statex.ullTotalVirtual / DIV);
+	//_tprintf(TEXT("There are %*I64d free  KB of virtual memory.\n"),
+	//	WIDTH, statex.ullAvailVirtual / DIV);
+	// Show the amount of extended memory available.
+	//_tprintf(TEXT("There are %*I64d free  KB of extended memory.\n"),
+	//	WIDTH, statex.ullAvailExtendedVirtual / DIV);
+	return;
 }
