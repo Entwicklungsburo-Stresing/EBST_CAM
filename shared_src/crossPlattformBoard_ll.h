195fd3bce6c4994fc30b899ad872472c044d599f
Florian Hahn <florian.hahn@posteo.de>
2021-05-27 13:10:30 +0200

    Fix windows crossPlattformBoard

--- shared_src/crossPlattformBoard_ll.h
+++ shared_src/crossPlattformBoard_ll.h
@@ -32,9 +32,10 @@ es_status_codes SetupDma( uint32_t drvno );
 es_status_codes enableInterrupt( uint32_t drvno );
 uint64_t getDmaAddress( uint32_t drvno);
 void ResetBufferWritePos(uint32_t drvno);
-void copyRestData(size_t rest_in_bytes);
+void copyRestData(uint32_t drvno, size_t rest_in_bytes);
 es_status_codes _InitBoard(uint32_t drvno);
 es_status_codes _InitDriver();
 es_status_codes _ExitDriver(uint32_t drvno);
 es_status_codes GetAddressOfPixel( uint32_t drvno, uint16_t pixel, uint32_t sample, uint32_t block, uint16_t CAM, uint16_t** address );
+
 #endif // CROSSPLATTFORMBOARDLL_H
