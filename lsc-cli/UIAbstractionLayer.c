e573d9a4a3a2610b136465881f95e694d928874b
Florian Hahn <florian.hahn@posteo.de>
2021-06-01 13:07:05 +0200

    Add lsc-cli

---
+++ lsc-cli/UIAbstractionLayer.c
@@ -0,0 +1,22 @@
+//UI abstraction layer for CCDExample
+#include "../shared_src/UIAbstractionLayer.h"
+
+void notifyMeasureStart( uint32_t drv )
+{
+	return;
+}
+
+void notifyMeasureDone( uint32_t drv )
+{
+	return;
+}
+
+void notifyBlockStart( uint32_t drv )
+{
+	return;
+}
+
+void notifyBlockDone( uint32_t drv )
+{
+	return;
+}
