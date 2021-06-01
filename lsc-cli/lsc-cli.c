076b8d013374ce749f6c7f517e61735f1190cbce
Florian Hahn <florian.hahn@posteo.de>
2021-06-01 13:26:22 +0200

    Change settings in lsc-cli.c for Lang

--- lsc-cli/lsc-cli.c
+++ lsc-cli/lsc-cli.c
@@ -4,7 +4,7 @@
 #include "../shared_src/crossPlattformBoard.h"
 
 #define DRVNO 1
-#define PIXEL 1088
+#define PIXEL 576
 
 int main(int argc, char **argv)
 {
@@ -18,13 +18,16 @@ int main(int argc, char **argv)
 	settings_struct.drvno = DRVNO;
 	settings_struct.board_sel = DRVNO;
 	settings_struct.bti_mode = bti_BTimer;
-	settings_struct.sti_mode = sti_STimer;
-	settings_struct.stime_in_microsec = 1000;
+	settings_struct.sti_mode = sti_I;
 	settings_struct.btime_in_microsec = 1000000;
-	settings_struct.nos = 1000;
+	settings_struct.nos = 500;
 	settings_struct.nob = 1;
-	settings_struct.camcnt = 1;
+	settings_struct.camcnt = 2;
 	settings_struct.pixel = PIXEL;
+	settings_struct.FFTMode = full_binning;
+	settings_struct.sensor_type = FFTsensor;
+	settings_struct.FFTLines = 64;
+	settings_struct.Vfreq = 7;
 
 	status = InitMeasurement(settings_struct);
 	if(status != es_no_error) return status;
