a90c162d05b8a4014614bee473424704a976abeb
Florian Hahn <florian.hahn@posteo.de>
2021-08-04 12:16:16 +0200

    Fix not all settingShowCameraBaseDir operations beeing bool

--- lsc-gui/mainwindow.h
+++ lsc-gui/mainwindow.h
@@ -52,7 +52,7 @@ private slots:
     void on_actionDump_board_registers_triggered();
 	void abortPressed();
 	void on_mychartView_rubberBandChanged();
-    void on_checkBoxShowCamera_stateChanged(int state, int camera);
+    void on_checkBoxShowCamera_stateChanged(bool state, int camera);
 	void on_pushButtonStart_pressed();
 };
 
