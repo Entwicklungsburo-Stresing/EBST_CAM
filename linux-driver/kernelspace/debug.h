5c7da1a497c9a732e1b36e9db85729b56f56612c
Florian Hahn <florian.hahn@posteo.de>
2021-11-10 17:25:42 +0100

    Prepare for building .deb packages

--- linux-driver/kernelspace/debug.h
+++ linux-driver/kernelspace/debug.h
@@ -13,9 +13,22 @@
 #ifndef _debug_h_
 #define _debug_h_
 
+#define MAX_PCIE_BOARDS 5
 
-#include "../userspace/constants.h"
+/* debugging */
+#define D_PCI            0x0001
+#define D_START_STOP     0x0002
+#define D_READOUT        0x0004
+#define D_INTERRUPT      0x0008
+#define D_BUFFERS        0x0010
+#define D_IOCTL          0x0020
+#define D_MODULE         0x0040
+#define D_MMAP           0x0080
+#define D_PROC           0x0100
+#define D_STATUS         0x0200
 
+#define DEBUG_BITS       0x03FF
+#define DEBUG_MASK_SHIFT 16
 
 #define PDEBUG(flags, ...) do {						  \
 	if ((dev->control && (dev->control->debug_mode & (flags)))	  \
