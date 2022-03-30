7c82f9906a7ef1ad4ae7b0c892805ebf8acb9868
Florian Hahn <florian.hahn@posteo.de>
2022-03-30 17:05:28 +0200

    Update copyright

--- version.h
+++ version.h
@@ -12,5 +12,5 @@
 
 #define VER_PRODUCT_VERSION         VER_FILE_VERSION
 #define VER_PRODUCT_VERSION_STR     VER_FILE_VERSION_STR
-#define VER_COPYRIGHT_STR           "Copyright EB Stresing (C) 2021"
+#define VER_COPYRIGHT_STR           "Copyright EB Stresing (C) 2022"
 #define VER_COMPANY_NAME            "EB Stresing - Berlin"
