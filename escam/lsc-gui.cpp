f700603d23e0423b3da6aae4b908830f6e147aa8
Florian Hahn <florian.hahn@posteo.de>
2023-01-09 11:11:57 +0100

    Fix indentation from spaces to tabs

--- escam/lsc-gui.cpp
+++ escam/lsc-gui.cpp
@@ -5,11 +5,11 @@ MainWindow* mainWindow;
 
 int main(int argc, char *argv[])
 {
-  QApplication app(argc, argv);
-  QCoreApplication::setOrganizationName("Entwicklungsbuero Stresing");
-  QCoreApplication::setOrganizationDomain("stresing.de");
-  QCoreApplication::setApplicationName("LSC");
-  mainWindow = new MainWindow;
-  mainWindow->show();
-  return app.exec();
+	QApplication app(argc, argv);
+	QCoreApplication::setOrganizationName("Entwicklungsbuero Stresing");
+	QCoreApplication::setOrganizationDomain("stresing.de");
+	QCoreApplication::setApplicationName("LSC");
+	mainWindow = new MainWindow;
+	mainWindow->show();
+	return app.exec();
 }
