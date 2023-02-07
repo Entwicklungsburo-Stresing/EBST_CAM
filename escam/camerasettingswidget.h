38953a7aae6221f756c6ca43ae7323e1efcd3c0b
Florian Hahn <florian.hahn@posteo.de>
2023-02-07 12:03:33 +0100

    Move settings level logic to camerasettingswidget

--- escam/camerasettingswidget.h
+++ escam/camerasettingswidget.h
@@ -7,6 +7,7 @@
 #include "ui_camerasettingswidget.h"
 #include "dialogsettings.h"
 #include <QDir>
+#include <QFileDialog>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class CameraSettingsWidgetClass; }
@@ -18,11 +19,29 @@ class CameraSettingsWidget : public QWidget
 public:
 	CameraSettingsWidget(QWidget *parent = nullptr);
 	~CameraSettingsWidget();
+	uint32_t drvno = 0;
 public slots:
 	void on_accepted();
+	void loadDefaults();
+	void changeSettingsLevel(int settings_level);
 private:
 	Ui::CameraSettingsWidgetClass *ui;
 	QSettings settings;
+	int _settings_level = 0;
+private slots:
+	void on_comboBoxSti_currentIndexChanged(int index);
+	void on_comboBoxBti_currentIndexChanged(int index);
+	void on_comboBoxSensorType_currentIndexChanged(int index);
+	void on_comboBoxCameraSystem_currentIndexChanged(int index);
+	void on_checkBoxRegionsEqual_stateChanged(int arg1);
+	void on_spinBoxPixel_valueChanged(int arg1);
+	void on_comboBoxFftMode_currentIndexChanged(int index);
+	void on_pushButtonFilePath_clicked();
+	void on_spinBoxLines_valueChanged(int value);
+	void on_spinBoxLinesBinning_valueChanged(int value);
+	void on_spinBoxNumberOfRegions_valueChanged(int value);
+	void on_checkBoxWriteDataToDisc_stateChanged(int arg1);
+	void on_checkBoxIsCooledCam_stateChanged(int arg1);
 };
 
 #endif // CAMERASETTINGSWIGET_H
