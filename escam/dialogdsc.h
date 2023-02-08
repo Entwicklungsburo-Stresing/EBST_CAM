40a63beb9ee144e4c42858217bcce73b25b29472
Florian Hahn <florian.hahn@posteo.de>
2023-02-08 15:39:29 +0100

    Transform dialogdsc to new board sel

--- escam/dialogdsc.h
+++ escam/dialogdsc.h
@@ -4,25 +4,25 @@
 #include <QDialog>
 
 namespace Ui {
-class DialogDSC;
+	class DialogDSC;
 }
 
 class DialogDSC : public QDialog
 {
-    Q_OBJECT
+	Q_OBJECT
 
 public:
-    explicit DialogDSC(QWidget *parent = nullptr);
-    ~DialogDSC();
+	explicit DialogDSC(QWidget* parent = nullptr);
+	~DialogDSC();
 
 private:
-    Ui::DialogDSC *ui;
+	Ui::DialogDSC* ui;
 private slots:
 	void on_pushButton_RS_DSC_1_clicked();
 	void on_pushButton_RS_DSC_2_clicked();
-    void on_comboBox_DIR_DSC_1_currentIndexChanged(int index);
-    void on_comboBox_DIR_DSC_2_currentIndexChanged(int index);
-	void on_pushButton_update_act_clicked();
+	void on_comboBox_DIR_DSC_1_currentIndexChanged(int index);
+	void on_comboBox_DIR_DSC_2_currentIndexChanged(int index);
+	void on_pushButton_update_cur_clicked();
 public slots:
 	void updateDSC();
 };
