3d0b231f033b633e8cfbb70e1f5cf818f6a2daf1
Florian Hahn <florian.hahn@posteo.de>
2023-03-23 15:53:12 +0100

    Fix possible loss of data with e.g. 510 scans
    Add WaitForAllInterruptsDone()

--- shared_src/globals.c
+++ shared_src/globals.c
@@ -34,6 +34,7 @@ uint64_t measurement_cnt = 0;
 char start_timestamp[file_timestamp_size];
 size_t temp_data_available[MAXPCIECARDS] = { 0, 0, 0, 0, 0 };
 volatile size_t* data_available = temp_data_available;
+volatile bool allInterruptsDone = false;
 const struct camera_settings camera_settings_default =
 {
 	.use_software_polling = settingsUseSoftwarePollingDefault,
