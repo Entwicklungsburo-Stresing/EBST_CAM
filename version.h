6d125b7c8917574224ea6b710f413b21be8f2422
Dennis Vollenweider <dennis-vollenweider@gmx.de>
2023-09-05 11:22:30 +0200

    Increase Version Number to 4.7.0

--- version.h
+++ version.h
@@ -2,8 +2,8 @@
 #define STRINGIZE(s) STRINGIZE2(s)
 
 #define VERSION_MAJOR_ESCAM         4  //Zaehlt nur alle paar jahre hoch, z.B. windows 20 mit 128 bit
-#define VERSION_PCIE_BOARD_VERSION  5 //PCI Karten Version
-#define VERSION_MINOR_ESCAM         1
+#define VERSION_PCIE_BOARD_VERSION  7 //PCI Karten Version
+#define VERSION_MINOR_ESCAM         0
 
 #define VER_FILE_VERSION            VERSION_MAJOR_ESCAM, VERSION_PCIE_BOARD_VERSION, VERSION_MINOR_ESCAM
 #define VER_FILE_VERSION_STR        STRINGIZE(VERSION_MAJOR_ESCAM)        \
@@ -12,5 +12,5 @@
 
 #define VER_PRODUCT_VERSION         VER_FILE_VERSION
 #define VER_PRODUCT_VERSION_STR     VER_FILE_VERSION_STR
-#define VER_COPYRIGHT_STR           "Copyright EB Stresing (C) 2022"
+#define VER_COPYRIGHT_STR           "Copyright EB Stresing (C) 2023"
 #define VER_COMPANY_NAME            "EB Stresing - Berlin"
