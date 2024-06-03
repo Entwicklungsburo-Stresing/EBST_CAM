c0c5f13a1be193a44121df3d2888565e31d53c5d
Florian Hahn <florian.hahn@stresing.de>
2024-06-03 12:25:36 +0200

    Format document

--- escam/crosshairs.h
+++ escam/crosshairs.h
@@ -7,13 +7,13 @@ class QChart;
 class Crosshairs
 {
 public:
-    Crosshairs(QChart* chart);
-    void updatePosition(QPointF position);
+	Crosshairs(QChart* chart);
+	void updatePosition(QPointF position);
 
 private:
-    QGraphicsLineItem* m_xLine, * m_yLine;
-    QGraphicsTextItem* m_xText, * m_yText;
-    QChart* m_chart;
+	QGraphicsLineItem* m_xLine, * m_yLine;
+	QGraphicsTextItem* m_xText, * m_yText;
+	QChart* m_chart;
 };
 
 #endif // CROSSHAIRS_H
