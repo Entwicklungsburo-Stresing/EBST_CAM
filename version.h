9527f20c69b99020c4babd1136887d9b5f458916
Florian Hahn <florian.hahn@stresing.de>
2024-06-12 16:18:01 +0200

    Increase version number to 4.12.0

--- version.h
+++ version.h
@@ -2,8 +2,8 @@
 #define STRINGIZE(s) STRINGIZE2(s)
 
 #define VERSION_MAJOR_ESCAM         4  //Zaehlt nur alle paar jahre hoch, z.B. windows 20 mit 128 bit
-#define VERSION_PCIE_BOARD_VERSION  11 //PCI Karten Version
-#define VERSION_MINOR_ESCAM         3
+#define VERSION_PCIE_BOARD_VERSION  12 //PCI Karten Version
+#define VERSION_MINOR_ESCAM         0
 
 #define VER_FILE_VERSION            VERSION_MAJOR_ESCAM, VERSION_PCIE_BOARD_VERSION, VERSION_MINOR_ESCAM
 #define VER_FILE_VERSION_STR        STRINGIZE(VERSION_MAJOR_ESCAM)        \
