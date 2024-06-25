3c01f36399dee5b3a67a4da4683d7a53e554fa35
Florian Hahn <florian.hahn@stresing.de>
2024-06-25 12:50:53 +0200

    Add documentation to makeall.ps1

--- makeall.ps1
+++ makeall.ps1
@@ -1,3 +1,7 @@
+# If you want to run this script, first you need to set the execution policy in a admin power shell:
+# Set-ExecutionPolicy -ExecutionPolicy Bypass
+# Also you need to add the path of devenv.com and MsBuild.exe to your environment variable PATH:
+# [Environment]::SetEnvironmentVariable("Path", $env:Path + ";C:\Program Files\Microsoft Visual Studio\2022\Professional\Common7\IDE;C:\Program Files\Microsoft Visual Studio\2022\Professional\MSBuild\Current\Bin", "Machine")
 devenv.com EBST_CAM.sln /Rebuild Release
 MSBuild.exe .\ESLSCDLL\ESLSCDLL.vcxproj /p:Configuration=Debug /p:Platform=x64
 MSBuild.exe .\ESLSCDLL\ESLSCDLL.vcxproj /p:Configuration=Debug_minimal /p:Platform=x64
