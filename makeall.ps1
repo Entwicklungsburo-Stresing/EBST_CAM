50e4b0ae543c1d8b2cd0afe1c90b813248db00b1
Florian Hahn <florian.hahn@stresing.de>
2024-07-17 12:25:20 +0200

    Fix typo

--- makeall.ps1
+++ makeall.ps1
@@ -1,4 +1,4 @@
-# If you want to run this script, first you need to set the execution policy in a admin power shell:
+# If you want to run this script, first you need to set the execution policy in an admin power shell:
 # Set-ExecutionPolicy -ExecutionPolicy Bypass
 # Also you need to add the path of devenv.com and MsBuild.exe to your environment variable PATH:
 # [Environment]::SetEnvironmentVariable("Path", $env:Path + ";C:\Program Files\Microsoft Visual Studio\2022\Professional\Common7\IDE;C:\Program Files\Microsoft Visual Studio\2022\Professional\MSBuild\Current\Bin", "Machine")
