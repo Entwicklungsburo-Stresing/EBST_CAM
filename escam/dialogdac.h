1907bd5a190bab709f102ecef489e73781a24644
Florian Hahn <florian.hahn@stresing.de>
2024-07-18 16:16:09 +0200

    Improve autotune
    Fix channel separated target reached. Change mean to double.

--- escam/dialogdac.h
+++ escam/dialogdac.h
@@ -30,8 +30,9 @@ private:
 	Ui::DialogDac *ui;
 	QSettings settings;
 	bool autotuneRunning = false;
+	bool allTargetsReached = false, ch1TargetReached = false, ch2TargetReached = false, ch3TargetReached = false, ch4TargetReached = false, ch5TargetReached = false, ch6TargetReached = false, ch7TargetReached = false, ch8TargetReached = false;
 	void autotunePressed();
-	int calculateMean(uint16_t* data, int start, int end, bool isHsIr);
+	double calculateMean(uint16_t* data, int start, int end, bool isHsIr);
 	bool autotuneAdjust(uint16_t* data, int start, int end, QSpinBox* spinBox, bool isHsir);
 };
 
