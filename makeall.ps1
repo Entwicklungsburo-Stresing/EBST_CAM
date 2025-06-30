f4a2e292d70750418e4dccac336cac9366f19239
Florian Hahn <florian.hahn@stresing.de>
2025-06-30 17:41:29 +0200

    Update release script

--- makeall.ps1
+++ makeall.ps1
@@ -3,51 +3,37 @@
 # Also you need to add the path of devenv.com and MsBuild.exe to your environment variable PATH:
 # [Environment]::SetEnvironmentVariable("Path", $env:Path + ";C:\Program Files\Microsoft Visual Studio\2022\Professional\Common7\IDE;C:\Program Files\Microsoft Visual Studio\2022\Professional\MSBuild\Current\Bin", "Machine")
 
-# Build Escam, DLL and setup in release config
-devenv.com EBST_CAM.sln /Rebuild Release
-# Build DLL all other configs
-MSBuild.exe .\ESLSCDLL\ESLSCDLL.vcxproj /p:Configuration=Debug /p:Platform=x64 /t:Rebuild
-MSBuild.exe .\ESLSCDLL\ESLSCDLL.vcxproj /p:Configuration=Debug_minimal /p:Platform=x64 /t:Rebuild
-MSBuild.exe .\ESLSCDLL\ESLSCDLL.vcxproj /p:Configuration=Debug-Labview /p:Platform=x64 /t:Rebuild
-MSBuild.exe .\ESLSCDLL\ESLSCDLL.vcxproj /p:Configuration=Release_minimal /p:Platform=x64 /t:Rebuild
-MSBuild.exe .\ESLSCDLL\ESLSCDLL.vcxproj /p:Configuration=Release-Labview /p:Platform=x64 /t:Rebuild
-# Recreate Release folder
-if(test-path Release)
-{
-	rm -r Release
-}
-mkdir Release
-# zip DLL
 $string = Get-Content .\version.h | Select-String -Pattern "#define VERSION_MAJOR_ESCAM"
 $major = $string -replace "[^0-9]", ''
 $string = Get-Content .\version.h | Select-String -Pattern "#define VERSION_PCIE_BOARD_VERSION"
 $pcie = $string -replace "[^0-9]", ''
 $string = Get-Content .\version.h | Select-String -Pattern "#define VERSION_MINOR_ESCAM"
 $minor = $string -replace "[^0-9]", ''
-mkdir Release\ESLSCDLL-$major.$pcie.$minor\
-mkdir Release\ESLSCDLL-$major.$pcie.$minor\Debug
-mkdir Release\ESLSCDLL-$major.$pcie.$minor\Debug_minimal
-mkdir Release\ESLSCDLL-$major.$pcie.$minor\Debug-Labview
-mkdir Release\ESLSCDLL-$major.$pcie.$minor\Release
-mkdir Release\ESLSCDLL-$major.$pcie.$minor\Release_minimal
-mkdir Release\ESLSCDLL-$major.$pcie.$minor\Release-Labview
-cp .\ESLSCDLL\x64\Debug\ESLSCDLL.dll .\Release\ESLSCDLL-$major.$pcie.$minor\Debug\
-cp .\ESLSCDLL\x64\Debug\ESLSCDLL.lib .\Release\ESLSCDLL-$major.$pcie.$minor\Debug\
-cp .\ESLSCDLL\x64\Debug\ESLSCDLL.pdb .\Release\ESLSCDLL-$major.$pcie.$minor\Debug\
-cp .\ESLSCDLL\x64\Debug_minimal\ESLSCDLL.dll .\Release\ESLSCDLL-$major.$pcie.$minor\Debug_minimal\
-cp .\ESLSCDLL\x64\Debug_minimal\ESLSCDLL.lib .\Release\ESLSCDLL-$major.$pcie.$minor\Debug_minimal\
-cp .\ESLSCDLL\x64\Debug_minimal\ESLSCDLL.pdb .\Release\ESLSCDLL-$major.$pcie.$minor\Debug_minimal\
-cp .\ESLSCDLL\x64\Debug-Labview\ESLSCDLL.dll .\Release\ESLSCDLL-$major.$pcie.$minor\Debug-Labview\
-cp .\ESLSCDLL\x64\Debug-Labview\ESLSCDLL.lib .\Release\ESLSCDLL-$major.$pcie.$minor\Debug-Labview\
-cp .\ESLSCDLL\x64\Debug-Labview\ESLSCDLL.pdb .\Release\ESLSCDLL-$major.$pcie.$minor\Debug-Labview\
-cp .\ESLSCDLL\x64\Release\ESLSCDLL.dll .\Release\ESLSCDLL-$major.$pcie.$minor\Release\
-cp .\ESLSCDLL\x64\Release\ESLSCDLL.lib .\Release\ESLSCDLL-$major.$pcie.$minor\Release\
-cp .\ESLSCDLL\x64\Release_minimal\ESLSCDLL.dll .\Release\ESLSCDLL-$major.$pcie.$minor\Release_minimal\
-cp .\ESLSCDLL\x64\Release_minimal\ESLSCDLL.lib .\Release\ESLSCDLL-$major.$pcie.$minor\Release_minimal\
-cp .\ESLSCDLL\x64\Release-Labview\ESLSCDLL.dll .\Release\ESLSCDLL-$major.$pcie.$minor\Release-Labview\
-cp .\ESLSCDLL\x64\Release-Labview\ESLSCDLL.lib .\Release\ESLSCDLL-$major.$pcie.$minor\Release-Labview\
-Compress-Archive -Path .\Release\ESLSCDLL-$major.$pcie.$minor\ -DestinationPath .\Release\ESLSCDLL-$major.$pcie.$minor.zip
+
+# Recreate Release folder
+if(test-path Release)
+{
+	rm -r Release
+}
+mkdir Release
+if(test-path .\ESLSCDLL\x64\)
+{
+	rm -r .\ESLSCDLL\x64\
+}
+mkdir .\ESLSCDLL\x64\
+
+# Build ESLSCDLL
+cd ..\ESLSCDLL
+.\makeall.ps1
+cd ..\EBST_CAM
+## Copy ESLSCDLL
+cp -r ..\ESLSCDLL\Release\ESLSCDLL-$major.$pcie.$minor\* .\ESLSCDLL\x64\
+
+# Build Escam, and setup in release config
+devenv.com EBST_CAM.sln /Rebuild Release
+
+cp -r ..\ESLSCDLL\Release\* .\Release\
 # zip setup
 mkdir Release\Escam-setup-$major.$pcie.$minor\
-cp .\escam_setup\Release\* .\Release\Escam-setup-$major.$pcie.$minor\
+cp -r .\escam_setup\Release\* .\Release\Escam-setup-$major.$pcie.$minor\
 Compress-Archive -Path .\Release\Escam-setup-$major.$pcie.$minor\ -DestinationPath .\Release\Escam-setup-$major.$pcie.$minor.zip
