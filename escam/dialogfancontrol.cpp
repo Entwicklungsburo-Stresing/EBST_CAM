d4c976e4533be45f511cb0c8d23667f00bcd5c26
Dennis Vollenweider <dennis-vollenweider@gmx.de>
2025-10-27 16:14:46 +0100

    Add Fan Control window logic
    Add window logic, which writes the fan state based on the state of the checkbox

--- escam/dialogfancontrol.cpp
+++ escam/dialogfancontrol.cpp
@@ -2,10 +2,31 @@
 
 DialogFanControl::DialogFanControl(QWidget *parent)
 	: QDialog(parent)
+	, ui(new Ui::DialogFanControlClass())
 {
-	ui.setupUi(this);
+	ui->setupUi(this);
 }
 
 DialogFanControl::~DialogFanControl()
-{}
+{
+	delete ui;
+}
+
+void DialogFanControl::on_checkBoxFanControlSet_stateChanged(int state)
+{
+	if (state == Qt::CheckState::Checked)
+		settings.setValue(settingMonitorPath, monitor_Vin_Fan_control);
+	return;
+}
+
+void DialogFanControl::on_checkBoxFanOn_stateChanged(int state)
+{
+	
+	uint32_t drvno = ui->spinBoxPcie->value();
+	uint16_t fanState = (state == Qt::CheckState::Checked) ? 1 : 0;
+	mainWindow->lsc.setFanControlState(drvno, fanState);
+	return;
+}
+
+
 
--- escam/dialogfancontrol.h
+++ escam/dialogfancontrol.h
@@ -1,17 +1,36 @@
+/*****************************************************************//**
+ * @file		dialogfancontrol.h
+ * @brief		Dialog to control the fan.
+ * @author		Dennis Vollenweider
+ * @date		27.10.2025
+ * @copyright	Copyright Entwicklungsbuero Stresing. This software is release under the LPGL-3.0.
+ *********************************************************************/
+
 #pragma once
 
 #include <QDialog>
 #include "ui_dialogfancontrol.h"
+#include "lsc-gui.h"
+#include <QSettings>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class DialogFanControlClass; };
+QT_END_NAMESPACE
 
 class DialogFanControl : public QDialog
 {
 	Q_OBJECT
 
 public:
-	DialogFanControl(QWidget *parent = nullptr);
+	DialogFanControl(QWidget *parent = Q_NULLPTR);
 	~DialogFanControl();
 
+private slots:
+	void on_checkBoxFanControlSet_stateChanged(int state);
+	void on_checkBoxFanOn_stateChanged(int state);
+
 private:
-	Ui::DialogFanControlClass ui;
+	Ui::DialogFanControlClass *ui;
+	QSettings settings;
 };
 
